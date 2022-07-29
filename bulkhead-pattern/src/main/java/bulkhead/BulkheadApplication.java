f1a7f7dfc9c4f77af6cb31fdc2456e0115bbba7c
gabrielaramburu <gabrielaramburu@gmail.com>
2022-07-28 22:53:18 -0300

    trying to execute app in standanlone tomcat

--- bulkhead-pattern/src/main/java/bulkhead/BulkheadApplication.java
+++ bulkhead-pattern/src/main/java/bulkhead/BulkheadApplication.java
@@ -2,21 +2,26 @@ package bulkhead;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class BulkheadApplication extends SpringBootServletInitializer{
-	
-	
+public class BulkheadApplication extends SpringBootServletInitializer {
+
 	public static void main(String[] args) {
-		SpringApplication.run(BulkheadApplication.class, args); 
+		SpringApplication.run(BulkheadApplication.class, args);
 		System.out.println("Ejecutando...");
 	}
-	
+
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BulkheadApplication.class);
 	}
 }
