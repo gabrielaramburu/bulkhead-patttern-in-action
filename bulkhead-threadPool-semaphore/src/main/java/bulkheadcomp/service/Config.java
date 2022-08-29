f8ae7ee235f2cffb756fc88b1821c31380612ec6
gabrielaramburu <gabrielaramburu@gmail.com>
2022-08-29 18:13:04 -0300

    diagrams and test cases

--- bulkhead-threadPool-semaphore/src/main/java/bulkheadcomp/service/Config.java
+++ bulkhead-threadPool-semaphore/src/main/java/bulkheadcomp/service/Config.java
@@ -9,21 +9,19 @@ import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
-public class Config  {
-//	implements AsyncConfigurer {
-//}
-//	
-//	@Bean(name = "threadPoolExecutor")
-//	public Executor getAsyncExecutor() {
-//		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//		executor.setCorePoolSize(7);
-//		executor.setMaxPoolSize(42);
-//		executor.setQueueCapacity(11);
-//		executor.setThreadNamePrefix("threadPoolExecutor-");
-//		executor.initialize();
-//		return executor;
-//	}
-//	
+public class Config implements AsyncConfigurer {
+
+	@Bean(name = "threadPoolExecutor")
+	public Executor getAsyncExecutor() {
+		ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+		executor.setCorePoolSize(7);
+		executor.setMaxPoolSize(42);
+		executor.setQueueCapacity(11);
+		executor.setThreadNamePrefix("threadPoolExecutor-");
+		executor.initialize();
+		return executor;
+	}
+	
 //	@Bean(name = "simpleAsyncTaskExecutor")
 //	public Executor getAsyncExecutor() {
 //		SimpleAsyncTaskExecutor executor = new SimpleAsyncTaskExecutor();
